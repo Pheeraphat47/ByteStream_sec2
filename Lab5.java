02acb46a485f2e864e0180fbd631597f5f41a729
Pheeraphat Dherachaisuphakij <107838345+Pheeraphat47@users.noreply.github.com>
2023-05-01 14:45:08 +0700

    Add files via upload

---
+++ Lab5.java
@@ -0,0 +1,38 @@
+package bytestreams2;
+
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Lab5 {
+
+    public static void main(String[] args) {
+
+        Student s = new Student("65130500053", "Pheez");
+
+        try {
+
+            FileOutputStream file = new FileOutputStream("data.dat");
+            ObjectOutputStream output = new ObjectOutputStream(file);
+            output.writeObject(s);
+
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Lab5.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Lab5.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        try {
+            FileInputStream file = new FileInputStream("data.dat");
+            ObjectInputStream input = new ObjectInputStream(file);
+            Student ss = (Student) input.readObject();
+            System.out.println(ss);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Lab5.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException | ClassNotFoundException ex) {
+            Logger.getLogger(Lab5.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+}
